f028858e29f0239440141c1db5c60a37b0750cb2
fabiantheblind <icke@fabianmoronzirfas.me>
2014-09-09 12:14:50 +0200

    more info in the webhook

--- webhook.php
+++ webhook.php
@@ -1,6 +1,10 @@
 <?php
 // chdir("source-files/"); // Move into the repository directory
-exec("git pull origin master"); // Get changes from the remote repository
+$output = [];
+$return_value = [];
+exec("git pull origin master",$output , $return_value); // Get changes from the remote repository
+$msg = "Output:\n" .implode("\n",$output) ."\nReturn value: " . implode("\n", $return_value);
+mail("fabiantheblind@gmail.com", "webhook picotor executed",$msg);
 // exec("git reset --hard origin/master"); // Update working copy, discarding all local changes
 // exec("export GEM_HOME=/gem/home/path; /gem/home/path/bin/jekyll ../../site"); // Run Jekyll
 ?>
